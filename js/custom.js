5afc20456d9a43c45a539b6ef80b045dda99252e
Davide Truffa <davide.truffa@gmail.com>
2017-04-27 13:02:12 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -11,22 +11,6 @@ $(document).ready(function() {
 	});
 });
 
-// google custom search
-function load_gcs() {
-	google.load('search', '1', {language : 'it', style : google.loader.themes.MINIMALIST});
-	google.setOnLoadCallback(function() {
-		var customSearchOptions = {};
-		var imageSearchOptions = {};
-		imageSearchOptions['layout'] = google.search.ImageSearch.LAYOUT_POPUP;
-		customSearchOptions['enableImageSearch'] = true;
-		customSearchOptions['imageSearchOptions'] = imageSearchOptions;
-		customSearchOptions['adoptions'] = {'layout': 'noTop'};
-		var customSearchControl = new google.search.CustomSearchControl('011562108841634451655:ebasebf20is', customSearchOptions);
-		customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-		customSearchControl.draw('search');
-	}, true);
-}
-
 // google analytics
   (function(i,s,o,g,r,a,m) {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
